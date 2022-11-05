07489b71bd8e7d175ec6cf3ac3892da6803495da
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-11-05 19:02:41 +0330

    add form changeHandler

--- src/components/pages/NewComment/NewComment.js
+++ src/components/pages/NewComment/NewComment.js
@@ -1,20 +1,31 @@
-import styles from "./NewComment.module.css"
+import { useState } from "react";
+import styles from "./NewComment.module.css";
 
 const NewCommentPage = () => {
+  const [comment, setComment] = useState({
+    name: "",
+    email: "",
+    body: "",
+  });
+
+  const changeHandler = (e) => {
+    setComment({ ...comment, [e.target.name]: e.target.value });
+  };
+
   return (
     <section className={styles.newComment}>
       <form className={styles.form}>
         <div>
-            <label htmlFor="name">Name : </label>
-            <input type="text" id="name" name="name"/>
+          <label htmlFor="name">Name : </label>
+          <input type="text" id="name" name="name" onChange={changeHandler} />
         </div>
         <div>
-            <label htmlFor="email">Email : </label>
-            <input type="text" id="email" name="email"/>
+          <label htmlFor="email">Email : </label>
+          <input type="text" id="email" name="email" onChange={changeHandler} />
         </div>
         <div>
-            <label htmlFor="body">New Comment :</label>
-            <textarea id="body" name="body"></textarea>
+          <label htmlFor="body">New Comment :</label>
+          <textarea id="body" name="body" onChange={changeHandler}></textarea>
         </div>
         <button type="submit">Add New Comment</button>
       </form>
