536e3a81254c7c6d24708ecb5c68b19a1ce877dd
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-11-05 18:58:20 +0330

    create new comment form

--- src/components/pages/NewComment/NewComment.js
+++ src/components/pages/NewComment/NewComment.js
@@ -1,7 +1,25 @@
+import styles from "./NewComment.module.css"
+
 const NewCommentPage = () => {
-    return ( 
-        <div>This is NewCommentPage</div>
-     );
-}
- 
+  return (
+    <section className={styles.newComment}>
+      <form className={styles.form}>
+        <div>
+            <label htmlFor="name">Name : </label>
+            <input type="text" id="name" name="name"/>
+        </div>
+        <div>
+            <label htmlFor="email">Email : </label>
+            <input type="text" id="email" name="email"/>
+        </div>
+        <div>
+            <label htmlFor="body">New Comment :</label>
+            <textarea id="body" name="body"></textarea>
+        </div>
+        <button type="submit">Add New Comment</button>
+      </form>
+    </section>
+  );
+};
+
 export default NewCommentPage;
